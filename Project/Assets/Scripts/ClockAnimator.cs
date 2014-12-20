7b0fab789dec5ea50ce4a3b808ecbf2845beb94d
Joel Kuntz <joel.kuntz.92@gmail.com>
2014-12-20 00:51:52 -0400

    First release

---
+++ Project/Assets/Scripts/ClockAnimator.cs
@@ -0,0 +1,45 @@
+﻿//http://catlikecoding.com/unity/tutorials/clock/
+using UnityEngine;
+using System;
+
+public class ClockAnimator : MonoBehaviour {
+
+	public Transform hours, minutes, seconds;
+	public bool analog;
+
+	private const float
+		hoursToDegrees = 360f / 12f,
+		minutesToDegrees = 360f / 60f,
+		secondsToDegrees = 360f / 60f;
+	
+	// Update is called once per frame
+	void Update () {
+
+		//continuos movement
+		if (analog) {
+			// Get current time
+			TimeSpan timespan = DateTime.Now.TimeOfDay;
+
+			// Rotate clock hands
+			hours.localRotation = Quaternion.Euler(0f,0f,(float)timespan.TotalHours * -hoursToDegrees);
+			minutes.localRotation = Quaternion.Euler(0f,0f,(float)timespan.TotalMinutes * -minutesToDegrees);
+			seconds.localRotation = Quaternion.Euler(0f,0f,(float)timespan.TotalSeconds * -secondsToDegrees);
+		}
+
+		// discrete steps (seconds)
+		else { 
+			// Get current time
+			DateTime time = DateTime.Now;
+
+			// Rotate clock hands
+			hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -hoursToDegrees);
+			minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -minutesToDegrees);
+			seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees);
+		}
+	}
+
+	public void ToggleAnalog()
+	{
+		analog = !analog;
+	}
+}
---
+++ Project/Assets/Scripts/TimeColor.cs
@@ -0,0 +1,87 @@
+﻿/* TimeColor.cs
+ * 
+ * Author:  Joel Kuntz - github.com/Frozenfire92
+ * Started: December 19 2014
+ * Updated: December 20 2014
+ * License: Public Domain
+ * 
+ * Attach this script to a Unity 4.6 UI/Text object.
+ * It will update the text with the time, hex value, and rgb
+ * afterwards it updates the main cameras background color to match it.
+ * 
+ * Inspired by http://whatcolourisit.scn9a.org/
+ * HexToColor from http://wiki.unity3d.com/index.php?title=HexConverter
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TimeColor : MonoBehaviour {
+
+	private struct RGB { public int r,g,b; }
+
+	private DateTime time;
+	private Camera cam;
+	private Text textObj;
+
+	private string doubleZero = "00";
+	private string hex;
+	private RGB rgb;
+	private Color32 color;
+
+	private bool toggleText = true;
+
+	void Start() 
+	{
+		// Set references
+		cam = Camera.main;
+		textObj = gameObject.GetComponent<Text>();
+	}
+
+	void Update () 
+	{
+		// Get current time
+		time = DateTime.Now;
+
+		// Reset and build hex string
+		hex = "";
+			// Hour
+		if (time.Hour == 0) hex += doubleZero;
+		else if (time.Hour > 0 && time.Hour < 10) hex += ("0" + time.Hour);
+		else hex += time.Hour;
+			// Minute
+		if (time.Minute == 0) hex += doubleZero;
+		else if (time.Minute > 0 && time.Minute < 10) hex += ("0" + time.Minute);
+		else hex += time.Minute;
+			// Second
+		if (time.Second == 0) hex += doubleZero;
+		else if (time.Second > 0 && time.Second < 10) hex += ("0" + time.Second);
+		else hex += time.Second;
+
+		// Get color, update text & camera
+		color = HexToColor(hex);
+		textObj.text = (toggleText) ? hex.Substring(0,2) + ":" + hex.Substring(2,2) + ":" + hex.Substring(4,2) + "\n#" + hex + "\nR" + rgb.r + " G" + rgb.g + " B" + rgb.b : "";
+		cam.backgroundColor = color;
+	}
+
+	// For UI to toggle the text on/off while maintaining camera color change
+	public void ToggleText ()
+	{
+		toggleText = !toggleText;
+	}
+
+	// Converts a string hex color ("123456") to Unity Color and sets the rgb int for text display
+	private Color32 HexToColor(string HexVal)
+	{
+		// Convert each set of 2 digits to its corresponding byte value
+		byte R = byte.Parse(HexVal.Substring(0,2), System.Globalization.NumberStyles.HexNumber);
+		byte G = byte.Parse(HexVal.Substring(2,2), System.Globalization.NumberStyles.HexNumber);
+		byte B = byte.Parse(HexVal.Substring(4,2), System.Globalization.NumberStyles.HexNumber);
+		// Save the rgb struct for text usage
+		rgb.r = R;
+		rgb.g = G;
+		rgb.b = B;
+		return new Color32(R, G, B, 255);
+	}
+}
